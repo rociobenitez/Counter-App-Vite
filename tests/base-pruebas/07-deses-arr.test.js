23646a7058db8490e67124f833d466c5a57c83cf
Rocio Benitez <rociobenitez2403@gmail.com>
2022-12-01 09:15:17 +0100

    First commit

---
+++ tests/base-pruebas/07-deses-arr.test.js
@@ -0,0 +1,9 @@
+import { retornaArreglo } from '../../src/base-pruebas/07-deses-arr';
+describe('Pruebas en 07-deses-arr', () =>{
+    test('debe retornar un string y un número', () =>{
+        const [ letters, numbers ] = retornaArreglo();
+        expect( letters ).toBe('ABC');
+        expect( numbers ).toBe(123);
+
+    })
+})
